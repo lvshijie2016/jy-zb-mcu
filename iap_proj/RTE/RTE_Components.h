133b2446ee0d92b4fed35d7906824395f2064c11
gaosheng1006 <gaosheng@unisound.com>
2017-12-11 20:17:05 +0800

    add ota uart commnication
    1.完成ota串口通讯协议
    2.flash读写调试中

--- iap_proj/RTE/RTE_Components.h
+++ iap_proj/RTE/RTE_Components.h
@@ -4,11 +4,17 @@
  *      *** Do not modify ! ***
  *
  * Project: 'KAR_v1.0' 
- * Target:  'KAR_v1.0' 
+ * Target:  'IAP' 
  */
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM0.h"
 
 
 #endif /* RTE_COMPONENTS_H */
